0f73fa4da76e413b60dccb5aa4f0a0fcfbafbd85
AleksandSP <36516562+AleksandSP@users.noreply.github.com>
2019-05-15 02:21:12 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import telebot
+
+token = "YOUR_Token"
+
+bot = telebot.TeleBot(token)
+
+upd = bot.get_updates();
+@bot.message_handler(commands=['help'])
+def handle_text(message) :
+    bot.send_message(message.chat.id, "Программа не работает если твой id не внесён в переменную myid,сделано для легкой персонализации пользователей ботом!")
+@bot.message_handler(commands=['start'])
+def handle_text(message):
+        bot.send_message(message.chat.id, "Программа работает только для активированных id")
+myid=YOUR_ID
+#(легко получить гэт апдейтом)
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    if((message.chat.type=="group") or (message.chat.type=="supergroup")) :
+        uniq(str(message.chat.id))
+    if(message.chat.id==myid):
+        f = open('text.txt', 'r')
+        for IdGroup in f:
+            chat_id=IdGroup
+            bot.forward_message(chat_id,message.chat.id,message.message_id)
+            print(chat_id+" /n")
+
+def uniq(id) :
+    f = open('text.txt', 'r')
+    for IdGroup in f:
+        if (IdGroup==id) :
+            id="1"
+    f.close()
+    if(id!="1"):
+        f = open('text.txt', 'a')
+        f.write(id+ '\n')
+        f.close()
+#with open('text.txt') as result:
+#uniqlines = set(result.readlines())
+#with open('text.txt', 'w') as rmdup:
+#rmdup.writelines(set(uniqlines))"""
+
+bot.polling(none_stop=True, interval=0)
